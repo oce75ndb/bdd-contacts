4cb1ca2658ac6de40904b29874f8d69621912cce
Océane Bondon <oceanebondon30@gmail.com>
2023-11-23 10:11:18 +0100

    insertion données 2

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -45,3 +45,16 @@ INSERT INTO pays (iso_3, nom, iso_2, nationalite) VALUES
 ('ITA', 'Italy', 'IT', 'Italian'),
 ('ESP', 'Spain', 'ES', 'Spanish'),
 ('BRA', 'Brazil', 'BR', 'Brazilian');
+
+-- Insertion des données dans 'contacts'
+INSERT INTO contacts (nom, prenom, date_de_naissance, sexe, adresse, cp, ville, pays_iso_3) VALUES
+('Doe', 'John', '1980-01-01', 'M', '123 Main St', '12345', 'New York', 'USA'),
+('Smith', 'Jane', '1985-02-02', 'F', '456 Elm St', '23456', 'Los Angeles', 'USA'),
+('Johnson', 'Robert', '1990-03-03', 'M', '789 Oak St', '34567', 'Chicago', 'USA'),
+('Williams', 'Emily', '1982-04-04', 'F', '101 Pine St', '45678', 'Houston', 'USA'),
+('Brown', 'Michael', '1987-05-05', 'M', '202 Maple St', '56789', 'Phoenix', 'USA'),
+('Davis', 'Sarah', '1984-06-06', 'F', '303 Cedar St', '67890', 'Philadelphia', 'USA'),
+('Wilson', 'Daniel', '1989-07-07', 'M', '404 Birch St', '78901', 'San Antonio', 'USA'),
+('Anderson', 'Lisa', '1986-08-08', 'F', '505 Redwood St', '89012', 'San Diego', 'USA'),
+('Garcia', 'Carlos', '1995-09-09', 'M', '606 Pineapple St', '90123', 'Miami', 'USA'),
+('Martinez', 'Isabella', '1998-10-10', 'F', '707 Orange St', '01234', 'San Francisco', 'USA');
