2a8a427ac72bf08a079387f96582f7d00e067a93
Océane Bondon <oceanebondon30@gmail.com>
2023-11-23 09:59:29 +0100

    creation table contacts

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -9,3 +9,17 @@ CREATE TABLE IF NOT EXISTS pays (
     iso_2 VARCHAR(2) NOT NULL,
     nationalite VARCHAR(50) NOT NULL
 );
+
+-- Creation d'une table 'contacts'
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(70) NOT NULL,
+    prenom VARCHAR(70) NOT NULL,
+    date_de_naissance DATE NOT NULL,
+    sexe VARCHAR(1) CHECK (sexe IN ('M', 'F')) NOT NULL,
+    adresse TEXT,
+    cp VARCHAR(10),
+    ville VARCHAR(70),
+    pays_iso_3 VARCHAR(3) NOT NULL,
+    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3)
+);
