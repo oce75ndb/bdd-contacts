2f1eb61659307c0d236df91db4df1571444fe372
Océane Bondon <oceanebondon30@gmail.com>
2023-11-23 09:58:37 +0100

    creation table pays

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -1,3 +1,11 @@
 -- Creation database
 CREATE DATABASE IF NOT EXISTS exo_contacts;
-USE exo_contacts; 
+USE exo_contacts;
+
+-- Creation d'une table 'pays'
+CREATE TABLE IF NOT EXISTS pays (
+    iso_3 VARCHAR(3) NOT NULL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    iso_2 VARCHAR(2) NOT NULL,
+    nationalite VARCHAR(50) NOT NULL
+);
